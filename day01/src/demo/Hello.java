58bef23bfb09383787e723cbd44d318f7fba251d
duanyanggg <3473213468@qq.com>
2026-03-18 19:32:10 +0800

    day01 homework

--- day01/src/demo/Hello.java
+++ day01/src/demo/Hello.java
@@ -1,4 +1,15 @@
 package demo;
 
 public class Hello {
+    public static void main(String[] args){
+        System.out.println("交通银行\n " +
+                "BANK OF COMMUNICATIONS\n " +
+                "您需要办理的业务是：现金汇款\n" +
+                "您的排队号是：\n" +
+                "Q3019\n" +
+                "您前面还有等待人数：11\n" +
+                "2014-10-15\t 09:39:05\n" +
+                "温馨提示：业务办理需要携带身份证等有效证件");
+    }
+
 }
