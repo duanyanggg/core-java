3a53529b480dc500f9f9f6e154338140eef16fd5
duanyanggg <3473213468@qq.com>
2026-03-19 11:23:52 +0800

    Keyboard shortcut and comment

---
+++ day02/src/demo/Demo1.java
@@ -0,0 +1,19 @@
+package demo;
+/*快捷键使用以及注释学习*/
+public class Demo1 {
+    // Ctrl + Alt + Shift + L 重新格式化
+    // Ctrl + D 复制当前行到下一行
+    // Ctrl + alt + up/down 移动代码
+    // Ctrl + v 删除当前行
+    // alt + insert 查找源码
+    // Ctrl + / 单行注释
+    // Ctrl + shift + / 多行注释
+
+
+    /*这是一个多行注释
+    hhhh*/
+
+    //文档注释
+    /** javadoc生成说明文档
+      */
+}
